498fb0c25c64e502ff3d81f0493f50a36f7f6dc9
analializardi884-hub <analia.lizardi884@students.cmcss.net>
2025-12-17 11:41:21 -0600

    Update task2.py

--- task2.py
+++ task2.py
@@ -1,1 +1,15 @@
+# Week 1: Chores and first purchase
+allowance += 4
+allowance += 10
+allowance -= 5
 
+# Week 2: Bonus week and purchase
+allowance ^= 3
+allowance += 7
+allowance += 8
+
+# Week 3: Savings
+saving deposit = 50
+
+# Print final allowance
+Allowance: 43.0
